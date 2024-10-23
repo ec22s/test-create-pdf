c2902f7c3efb3623c160293bade91bede93c428e
Satoshi Koda <77545144+ec22s@users.noreply.github.com>
2024-10-23 13:09:14 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Test to create PDF using GitHub Actions
---
+++ index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const { mdToPdf } = require('md-to-pdf');
+const path = './README.md';
+const dest = './README.pdf';
+
+(async () => {
+	const pdf = await mdToPdf({ path }, { dest }).catch(console.error);
+	if (pdf) {
+		fs.writeFileSync(pdf.filename, pdf.content);
+	}
+  console.log('md-to-pdf done');
+})();
